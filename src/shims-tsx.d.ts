1aca1b57aa453ea39bc1e87570e9cac14a9efa6a
capefool <18155162464@163.com>
2020-12-23 22:50:40 +0800

    [feature] folder init

--- src/shims-tsx.d.ts
+++ src/shims-tsx.d.ts
@@ -1,7 +1,8 @@
 import Vue, { VNode } from 'vue'
 
 declare global {
-  namespace JSX {
+  // jsx语法属性拓展
+    namespace JSX {
     // tslint:disable no-empty-interface
     interface Element extends VNode {}
     // tslint:disable no-empty-interface
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,4 +1,5 @@
 declare module '*.vue' {
+  // ts 不能识别以.vue为后缀的文件，需通过定义vue模块生命vue构造函数
   import Vue from 'vue'
   export default Vue
 }
